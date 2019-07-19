9993907fda5caa91079ee0649e9dd94e5c0b14bf
KassioVieira <kassio.vieira7@gmail.com>
2019-07-19 10:59:17 -0300

    Styles do componente

---
+++ Inputs/TextInputLayout/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components/native'
+import colors from '../../values/colors'
+
+export const WrapTextInput = styled.View`
+  margin-top:20;
+  padding-top:18;
+  width: 90%;
+  align-self: center;
+`
+
+export const Row = styled.View`
+  flex-direction: row;
+  justify-content: space-between
+`
+
+export const Label = styled.Text`
+`
+export const EditText = styled.TextInput`
+  width: 100%;
+  color: #000;
+  border-bottom-width: 1;
+  border-bottom-color: #555;
+`
